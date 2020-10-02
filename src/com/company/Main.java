b7617df8b764438a8365143954dbdc504b80054f
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-10-02 20:13:32 +0300

    Hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,75 +3,71 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        final double pi = 3.14;
-        double cat1 = 0, cat2;
-        cat1 = readValue(cat1);
-        cat2 = cat1;
 
+    public static void main(String[] args) {
 
-        System.out.print("Длина второго катета = ");
-        showInfo(cat2);
+        double cat1 = readValue();
+        double cat2 = calcCat2(cat1);
 
-        double c = calcHypot(cat1,cat2);
-        System.out.printf("Длина гипотенузы = %1$.2f ", c);
-        System.out.println();
-
-        double r = calcRadius(c);
-        System.out.printf("Радиус окружности равен = %1$.2f ", r);
-        System.out.println();
-
-        double s1 = calcAreaCircle(r);
-        System.out.printf("Для круга R = %1$.2f площадь s = %2$.2f%n",r,s1 );
-
-
-        double side = calcSideSquare(r);
-        System.out.printf("Сторона квдрата = %1$.2f\n", side);
-
-        double s2 = calcAreaSauqre(side);
-        System.out.printf("Площадь квдрата = %1$.2f%n", s2);
-
-        double diffAreas = areaPaintedPart(s1,s2);
-        System.out.printf("Плоащдь закрашенной части = %1$.2f", diffAreas);
-
+        logic(cat1,cat2);
     }
 
-    static double readValue(double cat1){
+
+    static double readValue() {
         Scanner num = new Scanner(System.in);
         System.out.println("Введите длину катета ");
-        cat1 = num.nextDouble();
-        return cat1;
+        return num.nextDouble();
     }
 
-    static double calcHypot (double cat1, double cat2)
-    {
-        return Math.sqrt(cat1 * cat1 + cat2 * cat2);
+    static double calcCat2(double cat1) {
+        double cat2 = cat1;
+        System.out.printf(" Длина второго катета = %1$.2f%n", cat2);
+        return cat2;
     }
 
-    static void showInfo( double a) {
-        System.out.println(a);
+    static double calcHypot(double cat1, double cat2) {
+        double c = Math.sqrt(cat1 * cat1 + cat2 * cat2);
+        System.out.printf("Длина гипотенузы = %1$.2f ", c);
+        return c;
     }
 
-    static double calcRadius(double c)
-    {
-        return c/2;
+    static double calcRadius(double c) {
+        double r = c/2;
+        System.out.printf("Радиус окружности равен = %1$.2f ", r);
+        return r;
     }
 
-    static double calcAreaCircle(double r)
-    {
-        return 3.14 * r * r;
+    static double calcAreaCircle(double r) {
+        double s1 = Math.PI * r * r;
+        System.out.printf("Для круга R = %1$.2f площадь s = %2$.2f%n", r, s1);
+        return s1;
+
     }
 
-    static double calcSideSquare ( double r){
-        return 2 * r;
+    static double calcSideSquare(double r) {
+        double side = 2 * r;
+        System.out.printf("Сторона квдрата = %1$.2f\n", side);
+        return side;
     }
 
-    static double calcAreaSauqre (double side){
-        return side * side;
+    static double calcAreaSquare(double side) {
+        double s2 = side * side;
+        System.out.printf("Площадь квдрата = %1$.2f%n", s2);
+        return s2;
     }
 
-    static double areaPaintedPart( double s1, double s2){
-        return s2 - s1;
+    static double areaPaintedPart(double s1, double s2) {
+        double diffAreas = s2-s1;
+        System.out.printf("Плоащдь закрашенной части = %1$.2f", diffAreas);
+        return diffAreas;
+    }
+
+    static void logic(double cat1,double cat2){
+        double c = calcHypot(cat1, cat2);
+        double r = calcRadius(c);
+        double s1 = calcAreaCircle(r);
+        double side = calcSideSquare(r);
+        double s2 = calcAreaSquare(side);
+        double diffAreas = areaPaintedPart(s1, s2);
     }
 }
